8184b03696d0a1ac8bb3df7f09233e60505923af
DESKTOP-TQK1JN8\recon <recon7347@gmail.com>
2021-02-14 21:39:44 +0900

    이동과 점프 구현

--- rhymer/Assets/02.Script/playerMovement.cs
+++ rhymer/Assets/02.Script/playerMovement.cs
@@ -8,8 +8,9 @@ public class playerMovement : MonoBehaviour
     public float movePower = 1f;
     public float jumpPower = 1f;
     Rigidbody2D rigid;
+    BoxCollider2D boxCollider2D;
     Vector3 movement;
-    bool isJumping=false;
+   
     SpriteRenderer spriteRenderer;
     //플레이어 이동을 위한 변수들 끝
 
@@ -19,6 +20,7 @@ public class playerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider2D=gameObject.GetComponent<BoxCollider2D>();
         rigid=gameObject.GetComponent<Rigidbody2D>();
         animator = gameObject.GetComponent<Animator>();
         spriteRenderer= gameObject.GetComponent<SpriteRenderer>();
@@ -39,15 +41,45 @@ public class playerMovement : MonoBehaviour
             animator.SetInteger("Direction",1);
             spriteRenderer.flipX=false;
         }
-        if(Input.GetButtonDown("Jump"))
+        
+        if(Input.GetAxisRaw("Horizontal")!=0)
         {
-            isJumping =true;
+            animator.SetBool("runState",true);
+            
+        }
+        else
+        {
+            animator.SetBool("runState",false);
+        }
+        if(Input.GetButtonDown("Jump")&& !animator.GetBool("isJumpping")) 
+        {
+            rigid.AddForce(Vector2.up*jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumpping",true);
         }
     }
     void FixedUpdate()
     {
         Move();
-        Jump();
+        //Jump();
+
+        //jump
+       
+        if(rigid.velocity.y<0 &&(animator.GetBool("isJumpping") ==true))
+        {
+            //Physics2D.Raycast(rigid.position,Vector3.down,1,LayerMask.GetMask("platform"))
+            RaycastHit2D rayHit = Physics2D.BoxCast(boxCollider2D.bounds.center,boxCollider2D.bounds.size,0f,Vector2.down,1f
+            ,LayerMask.GetMask("platform"));
+            //Debug.Log(rayHit.collider);
+            if(rayHit.collider != null)
+            {
+                Debug.Log(rayHit.distance);
+                if(rayHit.distance<0.17f)
+                {
+                    animator.SetBool("isJumpping",false);
+                }
+            }
+        }
+
     }
 
     void Move()
@@ -56,6 +88,7 @@ public class playerMovement : MonoBehaviour
         if(Input.GetAxisRaw("Horizontal")<0)
         {
             moveVelocity = Vector3.left;
+            
    
         }
         else if(Input.GetAxisRaw("Horizontal") > 0)
@@ -63,16 +96,18 @@ public class playerMovement : MonoBehaviour
             moveVelocity = Vector3.right;
 
         }
+        
+
         transform.position += moveVelocity * movePower * Time.deltaTime;
     }
 
-    void Jump()
-    {
-        if(!isJumping)return;
+    // void Jump()
+    // {
+    //     if(!isJumping)return;
 
-        rigid.velocity=Vector2.zero;
-        Vector2 jumpVelocity = new Vector2(0,jumpPower);
-        rigid.AddForce(jumpVelocity,ForceMode2D.Impulse);
-        isJumping = false;
-    }
+    //     rigid.velocity=Vector2.zero;
+    //     Vector2 jumpVelocity = new Vector2(0,jumpPower);
+    //     rigid.AddForce(jumpVelocity,ForceMode2D.Impulse);
+    //     isJumping = false;
+    // }
 }
