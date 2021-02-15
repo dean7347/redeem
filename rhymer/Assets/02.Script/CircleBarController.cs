8f0f0c61898b9ef970b4bec74c56c77cce05bfec
DESKTOP-TQK1JN8\recon <recon7347@gmail.com>
2021-02-15 10:28:52 +0900

    리듬노트 구현중

---
+++ rhymer/Assets/02.Script/CircleBarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleBarController : MonoBehaviour
+{
+    public GameObject CircleArea;
+    public GameObject fill;
+    public Vector2 growFill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        growFill = new Vector2(0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        fillerGrow();
+    }
+
+    void fillerGrow()
+    {
+        if(fill.transform.localScale.x <=1)
+        {
+            growFill = growFill+ new Vector2(0.1f*Time.deltaTime,0.1f*Time.deltaTime);
+            fill.transform.localScale=growFill;
+        }
+
+    }
+
+
+
+}
